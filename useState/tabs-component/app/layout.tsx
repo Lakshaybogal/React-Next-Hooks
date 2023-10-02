7b1d48b75dff2ec35585622ba7e3654b61177ef5
Lakshay Rajput <101181550+Lakshaybogal@users.noreply.github.com>
2023-10-02 11:35:51 +0530

    Update layout.tsx

--- useState/tabs-component/app/layout.tsx
+++ useState/tabs-component/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Toggle Button',
+  title: 'Tabs Component',
   description: 'Create by Lakshay Bogal',
 }
 
